5460599881f002746f9d0608bc909cb5007584b6
Ewerton Gustavo <141876652+Ewerton51@users.noreply.github.com>
2023-12-07 19:45:43 -0300

    Update HelloworldApplicationTests.java

--- HelloworldApplicationTests.java
+++ HelloworldApplicationTests.java
@@ -1,13 +1,28 @@
-package com.generation.helloworld;
+package com.generation.helloworld.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class HelloworldApplicationTests {
+@RestController 					
+@RequestMapping("/hello-world")		 
+public class HelloWorldController {
 
-	@Test
-	void contextLoads() {
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
 	}
-
+	
+	//endereço: http://localhost:8080/hello-world/bsms
+	@GetMapping("/bsms")	
+	public String generationBSM() {
+		return "Persistência, Mentalidade de crescimento, Responsabilidade, Orientação ao futuro, Proatividade, Comunicação, Trabalho em equipe, Orientação ao detalhe.";
+	}
+	
+	// endereço: http://localhost:8080/hello-world/lista
+	@GetMapping("/lista")
+	public String lista() {
+		return "Essa semana quero aprimorar meus conhecimentos em SQL e entender mais sobre HTTPS, vou complementar meus estudos de POO no final de semana, pois estou bem perdido nisso e sei que vou usar muito em breve";
+	}
+	
 }
